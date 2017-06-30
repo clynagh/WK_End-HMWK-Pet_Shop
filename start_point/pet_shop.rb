b16e50ce967eea21da406961fa180adcba696cbf
Craig Lynagh <craiglynagh@gmail.com>
2017-06-30 13:43:20 +0100

    tested & successful up to pets_sold

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,23 @@
+def pet_shop_name(shop_name)
+  return shop_name[:name]
+end
+
+def total_cash(sum)
+  return sum[:admin][:total_cash]
+end
+
+def add_cash(cash_add, addition)
+  return cash_add[:admin][:total_cash] += addition
+end
+
+def remove_cash(cash_add, addition)
+  return cash_add[:admin][:total_cash] -= addition
+end
+
+def pets_sold(pets_sold_number)
+  return pets_sold_number[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pets_sold_number, additonal_pets)
+
+end 
